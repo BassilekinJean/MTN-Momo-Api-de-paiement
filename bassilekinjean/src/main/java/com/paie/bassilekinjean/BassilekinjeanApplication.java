eaabb270770024311f3ead29816b8ff24a58cd7c
BassilekinJean <jean.bassilekin@facsciences-uy1.cm>
2025-08-24 01:03:31 +0100

    Ajout de la structure de base du projet avec Maven, y compris les fichiers de configuration, les dépendances et les classes principales pour l'application de paiement MTN Momo.

---
+++ bassilekinjean/src/main/java/com/paie/bassilekinjean/BassilekinjeanApplication.java
@@ -0,0 +1,13 @@
+package com.paie.bassilekinjean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BassilekinjeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BassilekinjeanApplication.class, args);
+	}
+
+}
---
+++ bassilekinjean/src/test/java/com/paie/bassilekinjean/BassilekinjeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paie.bassilekinjean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BassilekinjeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
